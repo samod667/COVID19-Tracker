da5b8d0d444d3d878b62c9f6ab95aee69979b333
dorsamoha <dorsamoha@gmail.com>
2020-06-22 17:56:29 +0300

    Updates

---
+++ precache-manifest.d44608c7efadce9f77f96d189498c008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1704bd22d178d055a9a14d0e10349e3",
+    "url": "/COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "d716c2a719ff30fd5db0",
+    "url": "/COVID19-Tracker/static/css/main.5f72f420.chunk.css"
+  },
+  {
+    "revision": "e936831dc96f72915d7b",
+    "url": "/COVID19-Tracker/static/js/2.4b3b91d8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19-Tracker/static/js/2.4b3b91d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d716c2a719ff30fd5db0",
+    "url": "/COVID19-Tracker/static/js/main.ec2ec7cb.chunk.js"
+  },
+  {
+    "revision": "5b2f52aa2bc7f1ce4280",
+    "url": "/COVID19-Tracker/static/js/runtime-main.1cbbe858.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19-Tracker/static/media/covid19.d7265326.png"
+  }
+]);
